bef10d4a7d7112945fce31ca4187ae4536670aa0
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-01-19 15:51:44 +0100

    first js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
+let products = document.querySelector(".products");
+let tube = document.querySelector(".tube");
 
+products.addEventListener("click", toggle);
+
+function toggle() {
+  products.classList.add("togglethis");
+  tube.classList.remove("tube");
+}
